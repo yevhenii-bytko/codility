9a06c399b0baed45d468d489226e0c4bddbbadc5
Yevhenii Bytko <yevhenii.bytko@gmail.com>
2020-12-14 14:26:02 +0100

    Perm check : first solution

---
+++ solutions/countings-elements/perm-check/perm_check.js
@@ -0,0 +1,21 @@
+function solution(A) {
+    const length = A.length;
+    const sum = ((length + 1) * length) / 2;
+
+    let sumOfValues = 0;
+    let counter = 0;
+    let foundElement = [];
+    let container = -1;
+
+    while (counter < length) {
+        container = A[container]; 
+
+        if (foundElement[container]) {
+            continue;
+        } else {
+            foundElement[container] = true;
+            sumOfValues += container;
+        }
+    }
+    return sum === sumOfValues ? 1 : 0;
+  }
